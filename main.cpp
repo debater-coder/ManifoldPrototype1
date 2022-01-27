c564575ecc96ac03ad223f7ab3c55e2dbb554e23
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-01-27 15:01:38 +1100

    Done

--- main.cpp
+++ main.cpp
@@ -8,9 +8,13 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 // Forward declarations:
 void drawImGuiStuff(GLFWwindow* window, ImGuiIO& io);
+void setTheme();
+
+auto showDemoWindow = false;
 
 int main(int argc, char* argv[]) {
 
@@ -20,7 +24,7 @@ int main(int argc, char* argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    auto mWindow = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+    auto mWindow = glfwCreateWindow(1200, 800, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
     if (mWindow == nullptr) {
@@ -39,16 +43,14 @@ int main(int argc, char* argv[]) {
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigDockingWithShift = true;
+    io.Fonts->AddFontFromFileTTF("OpenSans-Regular.ttf", 16);
     ImGui::StyleColorsDark();
+    setTheme();
     ImGui_ImplGlfw_InitForOpenGL(mWindow, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
-
-        // Input
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
 
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
@@ -78,21 +80,60 @@ int main(int argc, char* argv[]) {
 void drawImGuiStuff(GLFWwindow* window, ImGuiIO& io)
 {
     ImGui::DockSpaceOverViewport();
-    // ImGui::ShowDemoWindow();
-    ImGui::Begin("Hello World");
-    ImGui::End();
+    if (showDemoWindow)
+        ImGui::ShowDemoWindow(&showDemoWindow);
+
     if (ImGui::BeginMainMenuBar())
     {
-        if (ImGui::BeginMenu("Edit"))
+        if (ImGui::BeginMenu("File"))
         {
-            if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
-            if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+
+            if (ImGui::MenuItem("New")) {}
+            if (ImGui::MenuItem("Open")) {}
+            
+            ImGui::Separator();    
+            
+            if (ImGui::MenuItem("Save", "CTRL+S")) {}
+            if (ImGui::MenuItem("Save as", "CTRL+SHIFT+S")) {}  // Disabled item
+
             ImGui::Separator();
-            if (ImGui::MenuItem("Cut", "CTRL+X")) {}
-            if (ImGui::MenuItem("Copy", "CTRL+C")) {}
-            if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+
+            if (ImGui::MenuItem("Exit")) glfwSetWindowShouldClose(window, true);
+
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("Help"))
+        {
+
+            if (ImGui::MenuItem("Demo")) {
+                showDemoWindow = true;
+            }
+
             ImGui::EndMenu();
         }
         ImGui::EndMainMenuBar();
     }
+
+    ImGui::Begin("Hello World");
+    ImGui::Text("Hello I am text!");
+    ImGui::End();
+
+    ImGui::Begin("Boring Window");
+    ImGui::Text("Hello I am boring window.");
+    ImGui::End();
+
+    ImGui::Begin("Console");
+    ImGui::Text("Hello I am important console.");
+    ImGui::End();
+}
+
+void setTheme()
+{
+    ImGui::GetStyle().WindowRounding = 4.0f;// <- Set this on init or use ImGui::PushStyleVar()
+    ImGui::GetStyle().ChildRounding = 4.0f;
+    ImGui::GetStyle().FrameRounding = 4.0f;
+    ImGui::GetStyle().GrabRounding = 4.0f;
+    ImGui::GetStyle().PopupRounding = 4.0f;
+    ImGui::GetStyle().ScrollbarRounding = 4.0f;
 }
